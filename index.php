7bcb6ddbe3181448ea7d769cf002ee15252608d3
Ernest4CU <Ernest_xin@outlook.com>
2016-04-21 16:51:52 +0800

    基于mvc框架添加smarty视图引擎的框架

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 	header("Content-type:text/html;charset=utf8");
-	require_once('function.php');
-	
-	$view = ORG('Smarty/','Smarty',array('left_delimiter'=>'{','right_delimiter'=>
-		'}','template_dir'=>'tpl','compile_dir'=>'template_c','cache_dir'=>'cache'));
+	require_once('function.php');//mvc功能函数引用
+	require_once('config.php');//配置文件引用
+	$view = ORG('Smarty/','Smarty',$viewconfig);
 	$view->assign('str','测试');
 	$view->display('test.tpl');
 ?>
--- template_c/2754861e2d8f230f567d8b21e1e83fb3c7b33e8a_0.file.test.tpl.php
+++ template_c/2754861e2d8f230f567d8b21e1e83fb3c7b33e8a_0.file.test.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-04-21 10:40:34
+/* Smarty version 3.1.29, created on 2016-04-21 10:47:30
   from "D:\Program Files\wamp\www\mvc\tpl\test.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_571892029972e5_16329869',
+  'unifunc' => 'content_571893a27bc938_44533760',
   'file_dependency' => 
   array (
     '2754861e2d8f230f567d8b21e1e83fb3c7b33e8a' => 
     array (
       0 => 'D:\\Program Files\\wamp\\www\\mvc\\tpl\\test.tpl',
-      1 => 1461227964,
+      1 => 1461228449,
       2 => 'file',
     ),
   ),
@@ -19,7 +19,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_571892029972e5_16329869 ($_smarty_tpl) {
-echo $_smarty_tpl->tpl_vars['str']->value;
+function content_571893a27bc938_44533760 ($_smarty_tpl) {
+echo ($_smarty_tpl->tpl_vars['str']->value).('变量调节器');
 }
 }
